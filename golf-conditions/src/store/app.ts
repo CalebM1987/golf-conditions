59de8914086fb865ea601c3283e0de5737a2b0a6
CalebM1987 <caleb.mackey@gmail.com>
2022-10-20 03:45:56 -0500

    feat: set up initial state

--- golf-conditions/src/store/app.ts
+++ golf-conditions/src/store/app.ts
@@ -1,3 +1,9 @@
-import { ref } from 'vue'
+import { GolfCourseFeature } from './../types/golf';
+import { ref, reactive } from 'vue'
+
 
 export const baseUrl = ref('http://127.0.0.1:8000')
+
+export const golfCourses = ref<GolfCourseFeature[]>([])
+
+export const selectedIds = ref<number[]>([])
