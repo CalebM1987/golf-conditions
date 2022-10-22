077a30275e202ece07ab2f2335b2bc1046de6fe8
CalebM1987 <caleb.mackey@gmail.com>
2022-10-21 21:49:07 -0500

    fix(aeris): fixed get_args and improved rating logic

--- app/routers/weather/aeris.py
+++ app/routers/weather/aeris.py
@@ -1,13 +1,12 @@
 import os
 import httpx
 import math
-from typing import Union
+from typing import Union, List
 from munch import munchify
 from dotenv import dotenv_values
-from .models import RatingResponse
+from .models import RatingResponse, WeatherConditionsResponse
 from .ratings import *
 from app.utils import get_timestamp, from_milliseconds
-import datetime
 
 dotfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), '.env')
 aries_env = dotenv_values(dotfile)
@@ -15,13 +14,24 @@ aries_env = dotenv_values(dotfile)
 # aries base url
 base = 'https://api.aerisapi.com'
 
-def get_args(f='json', **kwargs):
+def get_args(f='json', fields: Union[str, List[str]]=None, **kwargs):
     """pass in extra query string arguments"""
     CLIENT_ID = aries_env.get('AERIS_CLIENT_ID')
     CLIENT_SECRET = aries_env.get('AERIS_CLIENT_SECRET')
-    extra = '&'.join(['='.join([k,v]) for k,v in kwargs.items()])
-    b = f'format=json&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}'
-    return '&'.join([b, extra]).rstrip('&')
+    params = dict(
+        f=f,
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        **kwargs
+    )
+
+    # validate fields
+    if fields:
+        if isinstance(fields, list):
+            fields = ','.join(fields)
+        params['fields'] = fields 
+   
+    return params
 
 RATING_CATEGORIES = ['bad', 'poor', 'good', 'very good', 'excellent']
 RATINGS = dict((i+1, c) for i,c in enumerate(RATING_CATEGORIES))
@@ -43,7 +53,7 @@ weather_fields =  [
         'profile.tz'
     ]
 
-async def get_current_conditions(location: str, **kwargs):
+async def get_current_conditions(location: str, **kwargs) -> WeatherConditionsResponse:
     """query the aeris weather api to fetch conditions for a given location
 
     Args:
@@ -52,12 +62,12 @@ async def get_current_conditions(location: str, **kwargs):
     Returns:
         _type_: _description_
     """
-    kwargs['fields']= ','.join(weather_fields)
+    kwargs['fields']= weather_fields
 
-    url = f'{base}/conditions/summary/{location}?{get_args(**kwargs)}'
+    url = f'{base}/conditions/summary/{location}'
     
     async with httpx.AsyncClient() as client:
-        r = await client.get(url)
+        r = await client.get(url, params=get_args(**kwargs))
         return munchify(r.json())
 
 
@@ -73,6 +83,8 @@ async def get_golf_conditions(location: str, reverse=False) -> RatingResponse:
     # 4. dew point, under 50 is comfortable lower the better (15%)
 
     # rank is round(weightedScore / 20)
+
+    If temp is less than 40, the rating will automatically be set to 1
 
     Args:
         location (str): a place or lat/lng
@@ -92,24 +104,28 @@ async def get_golf_conditions(location: str, reverse=False) -> RatingResponse:
         _min, _max = 5, 1
         indexNames = RATINGS_REVERSED
     try:
-        resp = await get_current_conditions(location, to='+6hours', fields=','.join(weather_fields))
+        resp = await get_current_conditions(location, to='+6hours', fields=weather_fields)
         conditions = resp.response[0]
         period = conditions.periods[0]
 
         # get condition elements
         tempF = period.temp.avgF
-        windMph = period.windSpeed.avgMPH
-        precipMM = period.precip.avgMM
-        dewF = period.dewpoint.avgF
+        if tempF < 40:
+            rating = 1
+        
+        else:
+            windMph = period.windSpeed.avgMPH
+            precipMM = period.precip.avgMM
+            dewF = period.dewpoint.avgF
 
-        # get temp ratings
-        temp = get_temp_rating(tempF) * 25
-        wind = get_wind_rating(windMph, tempF) * 30
-        precip = get_precip_rating(precipMM) * 30
-        dew = get_dew_rating(dewF, tempF) * 15
+            # get temp ratings
+            temp = get_temp_rating(tempF) * 25
+            wind = get_wind_rating(windMph, tempF) * 30
+            precip = get_precip_rating(precipMM) * 30
+            dew = get_dew_rating(dewF, tempF) * 15
 
-        # get rank
-        rating = min(5, round(sum([temp, wind, precip, dew]) / 20)) 
+            # get rank
+            rating = min(5, round(sum([temp, wind, precip, dew]) / 20)) 
         if reverse:
             rating = 6 - (rating or 1)
             
