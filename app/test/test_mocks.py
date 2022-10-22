8c9710df8009e7bf084a1fe987ff98cfa3398c0a
CalebM1987 <caleb.mackey@gmail.com>
2022-10-21 21:50:13 -0500

    test: improved mock tests

--- app/test/test_mocks.py
+++ app/test/test_mocks.py
@@ -5,7 +5,8 @@ import json
 from pytest_httpx import HTTPXMock
 from unittest.mock import patch
 from app.routers.weather.models import RatingResponse
-from app.routers.weather.aeris import get_golf_conditions
+from app.routers.weather.aeris import get_golf_conditions, get_args
+from munch import munchify
 
 def load_mock(name='dallas') -> RatingResponse:
     """load the mock response
@@ -22,13 +23,34 @@ def load_mock(name='dallas') -> RatingResponse:
         json_file = os.path.join(json_dir, 'dallas.json')
 
     with open(json_file, 'r') as f:
-        return json.load(f)
+        return munchify(json.load(f))
 
+
+def test_get_args():
+    params = get_args(to='+6hours')
+    assert len(params.keys()) == 4
 
 @pytest.mark.asyncio
 async def test_good_rating(httpx_mock: HTTPXMock):
     httpx_mock.add_response(json=load_mock('dallas'))
     resp = await get_golf_conditions('dallas,tx')
-    # assert resp.response[0].indice.current.index == 5
-    # cannot get this to recognize my mock for some reason
-    assert True
+    assert resp.response[0].indice.current.index == 5
+
+@pytest.mark.asyncio
+async def test_good_rating_reversed(httpx_mock: HTTPXMock):
+    httpx_mock.add_response(json=load_mock('dallas'))
+    resp = await get_golf_conditions('dallas,tx', reverse=True)
+    assert resp.response[0].indice.current.index == 1
+
+@pytest.mark.asyncio
+async def test_fair_rating(httpx_mock: HTTPXMock):
+    httpx_mock.add_response(json=load_mock('echo'))
+    resp = await get_golf_conditions('echo,or')
+    assert resp.response[0].indice.current.index == 3
+
+@pytest.mark.asyncio
+async def test_poor_conditions(httpx_mock: HTTPXMock):
+    httpx_mock.add_response(json=load_mock('pleasant-valley'))
+    resp = await get_golf_conditions('pleasant valley,ak')
+    assert resp.response[0].indice.current.index == 1
+    
